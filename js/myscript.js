7f12729d48a8bb0a74731042638da92fedf33842
gabrielemafrica <gabriele.ga@live.it>
2023-05-04 19:00:47 +0300

    inserisco le img da js

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,59 @@
+//creo array di immagini
+
+
+const imgCarusel = ['01','02','03','04','05'];
+
+//prendo il contenitore
+
+const container_carusel = document.getElementById('container_carusel');
+
+
+//ciclo le immagini
+
+for (let i = 0; i < imgCarusel.length; i++) {
+    const elementImg = imgCarusel[i];    
+    console.log(elementImg);
+    //setto numero immagine
+    const numeroImg = i + 1;
+
+    //le inserisco nel contenitore
+    const insertImg = `<img src="img/${elementImg}.webp" alt="immagine${numeroImg}">`;
+    container_carusel.innerHTML += insertImg;
+}
+
+// seleziono le immagini
+const items = document.querySelectorAll('img');
+
+//setto un indice per segnare l'item attivo al momento
+
+let activeItem = 0;
+
+//setto una variabile per stabilire il primo elemento
+
+const primaImg = items.length - (items.length - 1);
+
+//disattivo tutte le immagini
+
+for (let i = 0; i < items.length; i++) {
+
+    //aggiungo classe item
+    items[i].classList.add('item');
+    
+}
+
+// mostro la prima immagine
+if (items) {
+    // seleziono le immagini
+    const items = document.querySelectorAll('img');
+
+    //aggiungo la classe active
+    items[activeItem].classList.add('active');
+}
+    
+
+
+
+/*
 //seleziono gli item per poterli gestire
 
 
@@ -40,4 +96,4 @@ next.addEventListener(
             }
         }
     }
-)
+)*/
